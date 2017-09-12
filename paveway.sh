a1e6eb054935625bbac5caeaa778819ec8b53d71
Kenny Rasschaert <kenny@kennyrasschaert.be>
2017-09-12 15:38:43 +0200

    Shellcheck doesn't like unquoted

--- paveway.sh
+++ paveway.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
- 
- 
+
 [[ $# == 0 ]] && echo "Usage: $0 [ssh-args] user@hostname" && exit 1
- 
-ssh-keygen -R $@
-ssh-keyscan -H $@ >> ~/.ssh/known_hosts
-sshpass -p 'f00' ssh-copy-id $@  -o StrictHostKeyChecking=no || sshpass -p 'bar' ssh-copy-id $@  -o StrictHostKeyChecking=no
+
+ssh-keygen -R "$@"
+
+ssh-keyscan -H "$@" >> ~/.ssh/known_hosts
+sshpass -p 'f00' ssh-copy-id "$@"  -o StrictHostKeyChecking=no || sshpass -p 'bar' ssh-copy-id "$@"  -o StrictHostKeyChecking=no
+
 [[ $? -ne 0 ]] && echo "Connect failed, can't continue" && exit 2
- 
-ssh $@
+
+ssh "$@"
