ed80fc8c9587a6f5385775d0ccfadbafdfb646e5
Kenny Rasschaert <kenny@kennyrasschaert.be>
2017-09-13 10:11:39 +0200

    Added file transfer

--- paveway.sh
+++ paveway.sh
@@ -13,8 +13,8 @@ usage() {
 
 # Remove any previously recorded host key for this host, and save the current one to the known_hosts file
 clean_host_keys() {
-  ssh-keygen -R "$1"
-  ssh-keyscan -H "$1" >> ~/.ssh/known_hosts
+  ssh-keygen -R "$REMOTEHOST"
+  ssh-keyscan -H "$REMOTEHOST" >> ~/.ssh/known_hosts
 }
 
 # Open and parse the configuration file
@@ -27,18 +27,33 @@ read_config() {
   [[ -z "$PAVEWAY_PASSWORDS" ]] && fail "Configuration file $CONFIG_FILE does not set the mandatory PAVEWAY_PASSWORDS variable."
 }
 
-# Try logging in to the remote host with each of the preconfigured passwords and copy the default ssh key
-copy_key() {
+# Try logging in to the remote host with each of the preconfigured passwords and transfer the default ssh key
+transfer_key() {
   for p in "${PAVEWAY_PASSWORDS[@]}"; do
-    sshpass -p "$p" ssh-copy-id "$1"  -o StrictHostKeyChecking=no && return
+    sshpass -p "$p" ssh-copy-id -f "$REMOTEHOST"  -o StrictHostKeyChecking=no && return
   done
   # If this code path is reached, none of the preconfigured passwords worked.
   fail "Connect failed, can't continue"
 }
 
+# Transfer files to the remote host
+transfer_files() {
+  # Check if any files need to be transfered at all
+  [[ -z "$XFER_FILES" ]] && return
+  for f in "${XFER_FILES[@]}"; do
+      # Check if the specified file exists in $PATH using which
+      full_f_path=$(which "$f" 2>/dev/null)
+      # Verify the result is an actual path, and not an alias
+      echo "$full_f_path" | grep -qP '^/'
+      [[ $? -eq 0 ]] || fail "Can't find file $f in \$PATH, so it can't be transferred."
+      rsync -qav "$full_f_path" "${REMOTEHOST}:/bin/"
+  done
+}
+
 [[ $# == 0 ]] && usage
 REMOTEHOST="$1"
 read_config
-clean_host_keys "$REMOTEHOST"
-copy_key "$REMOTEHOST"
+clean_host_keys
+transfer_key
+transfer_files
 ssh "$REMOTEHOST"
